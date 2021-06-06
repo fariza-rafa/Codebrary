3fe160a630379cccef980245116e2ae1c69b9c66
Fariza Mubashshara Rafa <65975757+fariza-rafa@users.noreply.github.com>
2021-06-06 20:14:38 +0600

    Add files via upload

---
+++ HackerRank-JavaScript/arrays.js
@@ -0,0 +1,15 @@
+function getSecondLargest(nums) {
+  let firstLargestNum = 0;
+  let secondLargestNum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > firstLargestNum) {
+      secondLargestNum = firstLargestNum;
+      firstLargestNum = nums[i];
+    }
+    if (nums[i] > secondLargestNum && nums[i] < firstLargestNum) {
+      secondLargestNum = nums[i];
+    }
+  }
+  return secondLargestNum;
+}
---
+++ HackerRank-JavaScript/countObjects.js
@@ -0,0 +1,9 @@
+function getCount(objects) {
+  let pairCount = 0;
+  for (let i = 0; i < objects.length; i++) {
+    if (objects[i].x === objects[i].y) {
+      pairCount++;
+    }
+  }
+  return pairCount;
+}
